482a51c745aee8d2783fe7f80c21dc3d6e769f62
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-12 17:48:40 -0400

    balls and strikes rendering on screen

--- testing-ii/src/App.js
+++ testing-ii/src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Display from './components/Display'
+
 import './App.css';
 
+
+// Count Rules
+// balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+// balls and strikes reset to 0 when a hit is recorded.
+// a foul increases strikes up to 2. With no strikes, a foul makes it 1 strike. With 1 strike, a foul makes it 2 strikes. With two strikes a foul has no effect, count stays at 2 strikes.
+
 class App extends Component {
+  state = {
+    balls: 0, //max 4 
+    strikes: 0 // max 3
+  }
+
+increaseBall = e => {
+  this.setState(prevState => {
+    return{
+      balls: prevState.balls++ 
+    }
+    
+  }
+  )
+}
+
+increaseStrike = e => {
+  this.setState(prevState => {
+    return{
+      strikes: prevState.strikes++
+    }
+    
+  }
+  )
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Display balls={this.state.balls} strikes={this.state.strikes} />
+          
       </div>
     );
   }
---
+++ testing-ii/src/components/Display.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// display the count of balls and strikes for the at-bat.
+// should be updated when the user records activity on the Dashboard component.
+
+function Display(props) {
+  
+
+  return (
+    <>
+      <h3>Batter Up!</h3>
+        <p> Balls {props.balls}</p>
+        <p> Strikes {props.strikes}</p>
+   
+    </>
+  );
+}
+
+export default Display;
+
+
+
+
+
